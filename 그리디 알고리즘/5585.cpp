112d00026474cf43a565eba27d3a8fbe1684b907
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-07-04 22:35:16 +0900

    5585
    5585

---
+++ 그리디 알고리즘/5585.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int coin[] = {500, 100 ,50, 10, 5, 1};
+
+int main(){
+    int money;
+    cin >> money;
+    int realMoney = 1000 - money;
+    int cnt=0;
+
+    for(int i=0; i <=5;i++){
+        cnt += realMoney/coin[i];
+        realMoney %= coin[i];
+    }
+
+    cout << cnt;
+
+
+}
