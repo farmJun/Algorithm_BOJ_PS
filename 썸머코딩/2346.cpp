5967658d2fa6534eba41c11585c3c5c67abcd533
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-07-10 23:03:23 +0900

    2346
    2346

---
+++ 썸머코딩/2346.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<pair<int,int>>balloon;
+
+void moveRight(int a){
+    for (int i = 0; i < a-1; i++) {
+        balloon.push_back(balloon.front());
+        balloon.erase(balloon.begin());
+    }
+}
+
+void moveLeft(int a){
+    a= -a;
+    for (int i = 0; i < a; i++) {
+        balloon.insert(balloon.begin(), balloon.back());
+        balloon.erase(balloon.end()-1);
+    }
+}
+
+
+int main(){
+    int n;
+    cin >>n;
+
+    for(int i=0;i<n;i++){
+        int a;
+        cin >> a;
+        balloon.push_back(pair<int,int>(i+1, a));
+
+    }
+
+    for(int i=0 ; i<n;i++){
+        cout << balloon.front().first << " ";
+        int index = balloon.front().second;
+        balloon.erase(balloon.begin());
+
+        if (index > 0) {
+            moveRight(index);
+        }
+        else {
+            moveLeft(index);
+        }
+    }
+}
