3d05ab60ff7c44116be5636204261d57b3986d0f
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-07-10 23:03:05 +0900

    2075
    2075

---
+++ 썸머코딩/2075.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int n;
+    cin >> n;
+    int arr[n*n];
+
+    for(int i=0;i<n*n;i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+n*n,greater<>());
+    cout << arr[n-1];
+}
