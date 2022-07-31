801326bafd2dca8f7711f2d7023042838f962dc1
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-07-31 21:37:34 +0900

    19638
    19638

---
+++ 썸머코딩/19638.cpp
@@ -0,0 +1,38 @@
+#include "iostream"
+#include "queue"
+
+using namespace std;
+
+int main() {
+
+
+    priority_queue<int, vector<int>, less<int>> giant;
+    int N, H, T;
+    cin >> N >> H >> T;
+
+    for (int i = 0; i < N; i++) {
+        int h;
+        cin >> h;
+        giant.push(h);
+    }
+
+    for (int i = 0; i < T; i++) {
+        int h = giant.top();
+
+        if (h < H) {
+            cout << "YES\n";
+            cout << i << "\n";
+            return 0;
+        } else if (h > 1) {
+            giant.pop();
+            giant.push(h / 2);
+        }
+    }
+
+    if (giant.top() >= H) {
+        cout << "NO\n" << giant.top();
+    } else {
+        cout << "YES\n" << T << "\n";
+    }
+
+}
