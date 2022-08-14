5f8073798f94caec34c9cab00116a7ea12182648
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-08-14 18:26:20 +0900

    2193
    2193

---
+++ 다이나믹 프로그래밍/2193.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+long long dp[91];
+
+int main() {
+
+    dp[1] = 1;
+    dp[2] = 1;
+    int n;
+    cin >> n;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[n];
+
+}
