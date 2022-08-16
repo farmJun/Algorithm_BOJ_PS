193cda507b011031c9f4cc51ca64c73974fcefe7
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-08-16 16:43:22 +0900

    2839
    2839

---
+++ 다이나믹 프로그래밍/9084.cpp
@@ -0,0 +1,41 @@
+#include "iostream"
+
+using namespace std;
+
+int dp[10001];
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    for(int i=0; i<t;i++){
+        fill(dp, dp+10001, 0);
+
+        int n, m;
+        cin >> n;
+
+        int coin[n];
+
+        for(int j=0;j<n;j++){
+            cin >> coin[j];
+        }
+        cin >> m;
+
+        dp[0] = 1;
+
+        for (int j = 0; j < n; j++) {
+            for (int k= coin[j]; k <= m; k++) {
+                dp[k] += dp[k - coin[j]];
+            }
+        }
+
+        cout << dp[m] << "\n";
+
+
+    }
+
+
+
+
+}
