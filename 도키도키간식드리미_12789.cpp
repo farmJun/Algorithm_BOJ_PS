4d392047df2275aacede65c66c445a6e2e774ca7
박준영 <wnsdud6969@naver.com>
2023-11-17 17:28:09 +0900

    도키도키간식드리미_12789

---
+++ 도키도키간식드리미_12789.cpp
@@ -0,0 +1,58 @@
+#include "iostream"
+#include "stack"
+#include "vector"
+#include "queue"
+
+using namespace std;
+
+int main() {
+    int n;
+    stack<int> waiting;
+    queue<int> student;
+    vector<int> after;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        student.push(a);
+    }
+
+    int now = 1;
+
+    while (!student.empty()) {
+        if (student.front() == now) {
+            after.push_back(now);
+            now++;
+            student.pop();
+        } else if (!waiting.empty() && waiting.top() == now) {
+            waiting.pop();
+            after.push_back(now);
+            now++;
+        } else {
+            waiting.push(student.front());
+            student.pop();
+        }
+    }
+
+
+    while (!waiting.empty()) {
+        after.push_back(waiting.top());
+        waiting.pop();
+    }
+    bool possible = true;
+
+    for (int i = 0; i < after.size() - 1; i++) {
+        if (after[i] > after[i + 1]) {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible) {
+        cout << "Nice";
+    } else {
+        cout << "Sad";
+    }
+
+}
