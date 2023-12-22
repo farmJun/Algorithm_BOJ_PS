8d027247a9a7aa06aa0543aae4a945f04afc48bd
박준영 <wnsdud6969@naver.com>
2023-12-23 02:02:30 +0900

    분해합_2231

---
+++ 분해합_2231.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        int sum = i;
+        int j = i;
+        while (j > 0) {
+            sum += j % 10;
+            j /= 10;
+        }
+        if (sum == n) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+
+}
