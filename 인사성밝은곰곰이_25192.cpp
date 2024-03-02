171e1cca2266c6c6d2093f4ba04d6b7faab181a4
박준영 <wnsdud6969@naver.com>
2024-03-02 22:09:02 +0900

    인사성밝은곰곰이_25192

---
+++ 인사성밝은곰곰이_25192.cpp
@@ -0,0 +1,27 @@
+#include "iostream"
+#include "map"
+
+using namespace std;
+
+int main() {
+    int N;
+    string s;
+    int cnt = 0;
+
+    cin >> N;
+    map<string, bool> user;
+
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        if (s == "ENTER") {
+            user.erase(user.begin(), user.end());
+        } else {
+            if (!user[s]) {
+                cnt++;
+                user[s] = true;
+            }
+        }
+
+    }
+    cout << cnt;
+}
